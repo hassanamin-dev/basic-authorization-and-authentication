b7df9664a44df20fe7bd170052ab9c22fe6691a7
Hassan Amin <124259472+hassanamin-dev@users.noreply.github.com>
2025-02-09 18:59:45 +0500

    delete image from cloudinary and database

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,6 +1,7 @@
 const Image = require('../model/image.model.js')
 const uploadToCloudinary = require('../helpers/cloudinaryHelper.js')
 const fs = require('fs')
+const cloudinary = require('../config/cloudinary.js')
 
 const uploadImage = async (req, res) =>{
     try {
@@ -66,4 +67,47 @@ const fetchAllImages = async (req, res)=>{
         })
     }
 }
-module.exports = {uploadImage, fetchAllImages}
+
+const deleteImageController = async (req, res)=> {
+    try {
+        const getCurrentIdofImageToBeDeleted = req.params.id;
+        const userId = req.info.userId;
+
+        const image = await Image.findById(getCurrentIdofImageToBeDeleted);
+
+        if(!image){
+            return res.status(404).json({
+                success: false,
+                message: "Image not found"
+            })
+        }
+
+        // check if the image is uploaded by the current user who is trying to delete the image
+        if(image.uploadedBy.toString() !== userId){
+                return res.status(400).json({
+                    success: false,
+                    message: "You are not authorized to delte this image"
+                })
+        }
+
+        // delete image first from the cloudinary
+        await cloudinary.uploader.destroy(image)
+
+        // delete this image from database
+        await Image.findByIdAndDelete(getCurrentIdofImageToBeDeleted);
+
+        res.status(200).json({
+            success: true,
+            message: "Image deleted successfully"
+        })
+        
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Some thing went wrong please try again"
+        })
+    }
+}
+module.exports = {uploadImage, fetchAllImages, deleteImageController}
--- routes/image.routes.js
+++ routes/image.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {authMiddleware} = require('../middleware/auth.middleware.js');
 const adminMiddleware = require('../middleware/admin.middleware.js');
 const uploadMiddleware = require('../middleware/upload.middleware.js')
-const {uploadImage, fetchAllImages} = require('../controllers/image.controller.js')
+const {uploadImage, fetchAllImages, deleteImageController} = require('../controllers/image.controller.js')
 
 
 const router = express.Router();
@@ -13,5 +13,8 @@ router.post('/upload', authMiddleware, adminMiddleware, uploadMiddleware.single(
 
 // get all the images
 router.get('/get',authMiddleware, fetchAllImages)
+
+// delete the image
+router.delete('/:id', authMiddleware, adminMiddleware, deleteImageController)
 
 module.exports = router
